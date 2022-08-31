98807d245173363170ee9f066f31984b56d53c94
@V_86Q <112209157+donhackKrud@users.noreply.github.com>
2022-08-31 20:09:01 +0300

    Add files via upload

---
+++ donhack.sh
@@ -0,0 +1,209 @@
+clear
+clear
+
+g='\033[1;32m'
+
+gg='\033[5;32m'
+
+ggg='\033[7;32m'
+
+gggg='\033[0;32m'
+
+p='\033[1;35m'
+
+pp='\033[5;35m'
+
+ppp='\033[7;35m'
+
+pppp='\033[0;35m'
+
+y='\033[1;33m'
+
+yy='\033[5;33m'
+
+yyy='\033[7;33m'
+
+yyyy='\033[0;33m'
+
+r='\033[1;31m'
+
+rr='\033[5;31m'
+
+rrr='\033[7;31m'
+
+rrrr='\033[0;31m'
+
+b='\033[1;34m'
+
+bb='\033[5;34m'
+
+bbb='\033[7;34m'
+
+bbbb='\033[0;34m'
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+n=install
+
+
+cd
+
+echo -e "$ggg http://Donahck.com $gggg \n \n"
+read -p "Password ------> " e
+cd
+
+if [ "$e" == "DON HACK" ]; then
+
+
+rm -rif /sdcard/payload5.zip
+
+rm -rif /sdcard/*/payload5.zip
+
+rm -rif /sdcard/*/*/payload5.zip
+
+rm -rif /sdcard/*/*/*/payload5.zip
+
+rm -rif /sdcard/*/*/*/*/payload5.zip
+
+rm -rif /sdcard/payload6.zip
+
+rm -rif /sdcard/*/payload6.zip
+
+rm -rif /sdcard/*/*/payload6.zip
+
+rm -rif /sdcard/*/*/*/payload6.zip
+
+rm -rif /sdcard/*/*/*/*/payload6.zip
+rm -rif /sdcard/payload7.zip
+
+rm -rif /sdcard/*/payload7.zip
+
+rm -rif /sdcard/*/*/payload7.zip
+
+rm -rif /sdcard/*/*/*/payload7.zip
+
+rm -rif /sdcard/*/*/*/*/payload7.zip
+
+
+
+cd $HOME/payload5/.tool/sem2
+bash tool.sh
+
+cd $HOME/payload5
+rm index.html
+wget ifconfig.me
+clear
+chat(){
+echo 'def kk(t):' > .sssss.py
+echo '   import sys, time' >> .sssss.py
+echo '   for txt in t + "\n":' >> .sssss.py
+echo '        sys.stdout.write(txt)' >> .sssss.py
+echo '        sys.stdout.flush()' >> .sssss.py
+echo '        time.sleep(9. / 240)' >> .sssss.py
+
+
+echo 'h = "              \033[1;33m[\033[1;32m*\033[1;33m] you welcome in {\033[1;36mpauload7\033[1;33m} py [\033[1;32mali.max\033[1;33m] "' >> .sssss.py
+echo 'kk(h)' >> .sssss.py
+python2 .sssss.py
+
+}
+chat2(){
+echo 'def kk(t):' > .ssss.py
+echo '   import sys, time' >> .ssss.py
+echo '   for txt in t + "\n":' >> .ssss.py
+echo '        sys.stdout.write(txt)' >> .ssss.py
+echo '        sys.stdout.flush()' >> .ssss.py
+echo '        time.sleep(4. / 240)' >> .ssss.py
+
+
+
+echo 'f = "    \033[1;32m [\033[1;33m30\033[1;32m] Continue my account {\033[1;34mFacebook\033[1;32m}"' >> .ssss.py
+echo 'y = "     \033[1;32m[\033[1;33m40\033[1;32m] Follow my channel {\033[1;31mYouTube\033[1;32m}"' >> .ssss.py
+echo 'kk(f)' >> .ssss.py
+echo 'kk(y)' >> .ssss.py
+python2 .ssss.py
+
+}
+
+echo -e 'Wa'$p'/'
+clear
+echo -e "$g"
+figlet  -f big "           @V_86Q   "
+chat
+echo -e "$blue       .::::::::::.                           .::::::::::."
+sleep 0.1
+echo -e "     .$cyan Welcome to----->{2019}$blue
+sleep 0.1    
+echo -e "     ____   ___  _   _   _   _    _    ____ _  __ 
+               |  _ \ / _ \| \ | | | | | |  / \  / ___| |/ / 
+               | | | | | | |  \| | | |_| | / _ \| |   | ' /  
+               | |_| | |_| | |\  | |  _  |/ ___ \ |___| . \  
+               |____/ \___/|_| \_| |_| |_/_/   \_\____|_|\_\ 
+sleep 0.1    
+echo -e "      
+sleep 0.1.   
+echo -e ".      
+sleep 0.1
+echo -e "          $g   payload  $blue  |::::::|_()__()_|::::::|       $g 7 $blue      
+sleep 0.1
+echo -e "                             
+sleep 0.1
+echo -e "   
+sleep 0.1
+echo -e "   
+sleep 0.1
+echo -e "                          $cyan   [90]help  $blue 
+sleep 0.1
+echo -e  
+sleep 0.1
+echo -e "                      $bluo p $green a $reset y $purple l $cyan o $yellow a $green d"
+chat2
+echo ""
+echo ""
+echo -e "$green+________________________________________________________+"
+echo -e "$cyan|  [1] metasploit ️|  $blue  [2] nmap | $purple  [3] dos attack  |             "
+echo -e "$yellow|  [4]   ngrok   |  $red  [5] virus ️️|  $reset [6]   Termux    |    "
+echo "     "
+echo -e "$purple|  [7] facebook   $blue      [8] Encrypt   $cyan [9]   tool root   |"
+echo -e " $green          --------------------------------------"
+echo -e "                     $red [$cyan"$cyan"1212$red]password"
+echo -e "                     $red [$cyan"$cyan"6666$red]Hide (faile/phato/Video/...)"
+echo -e "                     $red [$cyan"$cyan"7777$red]Dump area"
+echo -e "              $red [$cyan"$cyan"8888$red]Backup photo/Video"
+echo -e "              $red [$cyan"$cyan"9999$red]Backup photo/Video ($cyan root$red )"
+
+
+
+echo -e " $red "
+echo -e " [00] Exit           $green           [99] update.       [000] next"
+#echo -e "$green"
+echo -e "$cyan"
+echo -e "|---{$yellow my ip$cyan }---|" 
+echo -e "  $g (y)  $r  (n)"
+read -p "  " fff
+if [ "$fff" == "y" ]; then
+echo -e "$cyan"
+cat index.html
+echo -e "$blue"
+ifconfig wlan0 | grep -o 192..........
+#echo -e "$green "
+else
+echo -e "  $r hidden"
+fi
+else
+
+echo -e "$rrr erorr Password $rrrr"
+
+sleep 1
+
+payload
+
+fi
